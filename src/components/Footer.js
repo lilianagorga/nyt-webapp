3cbff14e26f5359eb25c238511a2f38b145dbf09
Liliana Gorga <gorgaliliana87@gmail.com>
2023-07-27 12:17:42 +0200

    fix padding

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,23 +3,24 @@ import '../styles/footer.css'
 
 const Footer = () => {
   return (
-    <footer>
-        <div className='footer-container'>
-          <span>Copyright &copy; 2023 New York Times</span>
-          <span>NYTCo</span>
-          <span>Contact Us</span>
-          <span>Accessibility</span>
-          <span>Work with us</span>
-          <span>Advertise</span>
-          <span>T Brand Studio</span>
-          <span>Your Ad Choices</span>
-          <span>Privacy Policy</span>
-          <span>Term of Service</span>
-          <span>Term of Sale</span>
-          <span>Site Map</span>
-          <span>Help</span>
-          <span>Subscriptions</span>
-        </div>
+    <footer>  
+      <div className='footer-container'>
+        <hr />
+        <span>Copyright &copy; 2023 New York Times</span>
+        <span>NYTCo</span>
+        <span>Contact Us</span>
+        <span>Accessibility</span>
+        <span>Work with us</span>
+        <span>Advertise</span>
+        <span>T Brand Studio</span>
+        <span>Your Ad Choices</span>
+        <span>Privacy Policy</span>
+        <span>Term of Service</span>
+        <span>Term of Sale</span>
+        <span>Site Map</span>
+        <span>Help</span>
+        <span>Subscriptions</span>
+      </div>
     </footer>
   );
 };
