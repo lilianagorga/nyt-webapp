0b5f6d0d237ab83f8218a14bb22dca82e700cfe0
Liliana Gorga <gorgaliliana87@gmail.com>
2023-07-31 15:17:25 +0200

    rename class image

--- src/components/ShowCard.js
+++ src/components/ShowCard.js
@@ -13,7 +13,7 @@ const ShowCard = ({ article }) => {
   return (
     <section>
         <article key={_id} className='show-article'>
-          <img src={imageUrl} alt={main} />
+          <img src={imageUrl} alt={main} className='show-image' />
           <div className='article-content'>
             <h2 className='show-main'>{main}</h2>
             <p className='show-abstrat'>{abstract}</p>
