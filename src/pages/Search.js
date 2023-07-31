610181147eb0b6eced2ce837b87d3210c64f0c5d
Liliana Gorga <gorgaliliana87@gmail.com>
2023-07-31 11:59:14 +0200

    removed not necessary information about article and state in component

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,39 +1,37 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import Header from '../components/Header';
-import SearchCard from '../components/SearchCard';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 
 
 const Search = () => {
-  const [articles, setArticles] = useState([]);
   const { term } = useParams();
   const key = process.env.REACT_APP_NYT_API_KEY;
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchArticles = async () => {
       try {
         let apiUrl;
         if (term.trim() === 'home') {
-          window.location.href = '/';
+          navigate('/');
         } else {
           apiUrl = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${term}&api-key=${key}`;
         }
         const res = await fetch(apiUrl);
         const data = await res.json();
-        setArticles(data.response.docs);
+        console.log(data.response.docs);
       } catch (error) {
         console.error(error);
       }
     };
     fetchArticles();
-  }, [term, key]);
+  }, [term, key, navigate]);
 
 
 
   return (
       <div>
         <Header />
-        <SearchCard articles={articles} />
       </div>
   );
 };
